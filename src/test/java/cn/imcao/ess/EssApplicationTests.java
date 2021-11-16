18e0d671192d1524a177e5f508813d119c1482bd
ImCaO <imcao@imcao.cn>
2021-11-16 10:51:54 +0800

    Initial commit

---
+++ src/test/java/cn/imcao/ess/EssApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.imcao.ess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
