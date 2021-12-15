712be5cad6782254470ac879e7e05299509946ed
ImCaO <imcao@imcao.cn>
2021-12-15 20:53:57 +0800

    encryption

--- src/test/java/cn/imcao/ess/EssApplicationTests.java
+++ src/test/java/cn/imcao/ess/EssApplicationTests.java
@@ -1,13 +1,26 @@
 package cn.imcao.ess;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EssApplicationTests {
 
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
     @Test
     void contextLoads() {
+
     }
 
+    private String encrypt(String originPassword) {
+        return stringEncryptor.encrypt(originPassword);
+    }
+
+    private String decrypt(String encryptedPassword) {
+        return stringEncryptor.decrypt(encryptedPassword);
+    }
 }
